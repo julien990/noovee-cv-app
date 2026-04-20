e92b5ba3824837ff0cc448e397a0c77ac5f1cba8
julien990 <julien@miint.pro>
2026-04-20 10:53:36 +0200

    Fix IS_CLOUD for Render

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ NO_DATA = "---"
 
 # Detection cloud vs local
 try:
-    IS_CLOUD = "gcp_service_account" in st.secrets
+    IS_CLOUD = "gcp_service_account" in st.secrets or os.environ.get("IS_CLOUD_OVERRIDE") == "true"
 except:
-    IS_CLOUD = False
+    IS_CLOUD = os.environ.get("IS_CLOUD_OVERRIDE") == "true"
 
 # Chemins
 PATH_LOCAL = "/Users/juliensac/Library/CloudStorage/GoogleDrive-julien@miint.pro/Drive partagés/Noovee - CV"
